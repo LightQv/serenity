eb8aac7a907a30b4edbc5d69198cc8b331d33dce
Jawad Messaoudene <[jawadmessaoudene@gmail.com]>
2023-06-21 14:43:56 +0200

    op work in progress

--- frontend/src/pages/admin/AdminOperation.jsx
+++ frontend/src/pages/admin/AdminOperation.jsx
@@ -1,9 +1,105 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const backEndUrl = import.meta.env.VITE_BACKEND_URL;
 
 export default function AdminOperation() {
+  const [operations, setOperations] = useState([]);
+  const [operationName, setOperationName] = useState({ operation_name: "" });
+  const [showInput, setShowInput] = useState(false);
+  const [operationToDelete, setOperationToDelete] = useState(null);
+  const [operationToEdit, setOperationToEdit] = useState(null);
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const fetchOperations = async () => {
+    try {
+      const response = await axios.get(`${backEndUrl}/api/operations`);
+      setOperations(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addOperation = async () => {
+    if (operationName.trim() === "") {
+      return;
+    }
+    try {
+      const response = await axios.post(`${backEndUrl}/api/operations`, {
+        operationName,
+      });
+      if (response.status === 201) {
+        setOperationName("");
+        fetchOperations();
+        setShowInput(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const confirmDelete = (operation) => {
+    setOperationToDelete(operation);
+  };
+
+  const cancelDelete = () => {
+    setOperationToDelete(null);
+  };
+
+  const deleteOperation = async () => {
+    if (!operationToDelete) {
+      return;
+    }
+
+    try {
+      const response = await axios.delete(
+        `${backEndUrl}/api/operations/${operationToDelete.id}`
+      );
+      if (response.status === 200) {
+        toast.success("Operation deleted successfully !");
+        fetchOperations();
+        setOperationToDelete(null);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const editOperation = (operation) => {
+    setOperationToEdit(operation);
+    setShowEditForm(true);
+  };
+
+  const saveEditedOperation = async () => {
+    if (!operationToEdit) {
+      return;
+    }
+    try {
+      const response = await axios.put(
+        `${backEndUrl}/api/operations/${operationToEdit.id}`,
+        operationToEdit
+      );
+      if (response.status === 200) {
+        fetchOperations();
+        setShowEditForm(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const cancelEdit = () => {
+    setOperationToEdit(null);
+    setShowEditForm(false);
+  };
+  useEffect(() => {
+    fetchOperations();
+  }, []);
+
   return (
     <main className="min-w-screen min-h-screen bg-slate-50 font-poppins lg:ml-60">
-      <h1 className="left-50 relative top-20  h-72 text-[48px] font-semibold leading-[72px]">
+      <h1 className="absolue h-72 text-[30px] font-semibold leading-[72px]">
         Gestion des opérations
       </h1>
       <div className="flex">
@@ -30,6 +126,92 @@ export default function AdminOperation() {
           required=""
           className="rounded-lg p-2 text-sm placeholder:italic placeholder:opacity-50"
         />
+      </div>
+      <div>
+        <div className="columns">
+          {operations.map((operation) => (
+            <div key={operation.id} className="column">
+              <p>{operation.operation_name}</p>
+              {operation.id && (
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => confirmDelete(operation)}
+                  >
+                    Supprimer
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => editOperation(operation)}
+                  >
+                    Edit
+                  </button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        {!showInput && (
+          <button type="button" onClick={() => setShowInput(true)}>
+            <span className="visually-hidden">Ajouter un operation</span>
+          </button>
+        )}
+        {showInput && (
+          <div>
+            <label htmlFor="operation_nameInput">Nom de l'opération:</label>
+            <input
+              type="text"
+              value={operationName}
+              onChange={(e) => setOperationName(e.target.value)}
+              placeholder="Nom de l'opération"
+            />
+            <button
+              className="ml-8 mr-8 mt-4 rounded-lg bg-violet-dark-0 p-2 text-base font-bold text-white"
+              type="button"
+              onClick={addOperation}
+            >
+              Ajouter une opération
+            </button>
+          </div>
+        )}
+        {operationToDelete && (
+          <div className="modal">
+            <div className="modal-content">
+              <p>Êtes-vous sûr de vouloir effacer cette opération ?</p>
+              <div>
+                <button type="button" onClick={deleteOperation}>
+                  Oui
+                </button>
+                <button type="button" onClick={cancelDelete}>
+                  Non
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {showEditForm && operationToEdit && (
+          <div className="edit-form">
+            <h2>Edit</h2>
+            <label htmlFor="editOperationNameInput">Nom de l'opération:</label>
+            <input
+              type="text"
+              id="editOperationNameInput"
+              value={operationToEdit.operation_name}
+              onChange={(e) => {
+                setOperationToEdit({
+                  ...operationToEdit,
+                  operation_name: e.target.value,
+                });
+              }}
+            />
+            <button type="button" onClick={saveEditedOperation}>
+              Save
+            </button>
+            <button type="button" onClick={cancelEdit}>
+              Cancel
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
