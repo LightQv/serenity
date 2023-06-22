f53ca9a791fab7e973ce8276e750e0103b9cdf6b
LightQv <QVivian23@gmail.com>
2023-06-22 14:06:04 +0200

    token cookie & user in localstorage

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,39 +1,33 @@
 // import some node modules for later
-
+const express = require("express");
 const fs = require("node:fs");
 const path = require("node:path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const router = require("./router");
 
 // create express app
-
-const express = require("express");
-
 const app = express();
 
 // use some application-level middlewares
-
-app.use(express.json());
-
-const cors = require("cors");
-
 app.use(
   cors({
     origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
     optionsSuccessStatus: 200,
+    credentials: true,
   })
 );
 
+app.use(express.json());
+app.use(cookieParser());
+
 // import and mount the API routes
-
-const router = require("./router");
-
 app.use(router);
 
 // serve the `backend/public` folder for public resources
-
 app.use(express.static(path.join(__dirname, "../public")));
 
 // serve REACT APP
-
 const reactIndexFile = path.join(
   __dirname,
   "..",
--- backend/src/router.js
+++ backend/src/router.js
@@ -8,12 +8,12 @@ const {
   hashPassword,
   verifyPassword,
   verifyToken,
+  logout,
 } = require("./services/auth");
 
-// Public Routes
+// Public Routes (Auth)
 router.post("/api/login", getUserByEmailMiddleware, verifyPassword);
-
-router.use(verifyToken);
+router.get("/api/logout", verifyToken, logout);
 
 // Private Routes
 const userControllers = require("./controllers/userControllers");
