6cc3093c65275a371afdbab64a8da348e95034bd
Aurélie <aurelie.demure.ad@gmail.com>
2023-06-23 11:15:23 +0200

    added delete request for patients

--- frontend/src/components/admin/patient/DeletePatient.jsx
+++ frontend/src/components/admin/patient/DeletePatient.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import APIService from "../../../services/APIService";
-
-import { notifyError } from "../../../services/ToastNotificationService";
+import notifySuccess, {
+  notifyError,
+} from "../../../services/ToastNotificationService";
 
 export default function DeletePatient() {
   const { id } = useParams();
 
-  const [editPatient, setEditPatient] = useState({
+  const [deletePatient, setDeletePatient] = useState({
     firstname: "",
     lastname: "",
     email: "",
@@ -21,14 +22,31 @@ export default function DeletePatient() {
   });
 
   useEffect(() => {
-    APIService.delete(`/users/${id}`, editPatient)
-      .then((response) => setEditPatient(response.data))
+    APIService.delete(`/users/${id}`, deletePatient)
+      .then((response) => {
+        setDeletePatient(response.data);
+        notifySuccess("Le patient a été supprimé");
+      })
       .catch((err) => {
         if (err.request.status === 401) {
           notifyError("La requete a échouée");
         }
       });
   }, []);
+
+  const handleDelete = async () => {
+    try {
+      const res = await APIService.delete(`/users/${id}`, deletePatient);
+      if (res) {
+        notifySuccess("Le patient a été supprimé");
+      }
+      throw new Error();
+    } catch (error) {
+      if (error.request?.status === 500) {
+        notifyError("La requête a échouée.");
+      }
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-between p-4 lg:p-8">
@@ -39,7 +57,7 @@ export default function DeletePatient() {
         <button
           type="button"
           className="my-4 h-fit w-fit self-center rounded-lg border-2 border-violet-dark-0 bg-violet-dark-0 px-6 py-3 text-sm text-slate-100 shadow-lg transition-all hover:border-violet-light-0 hover:bg-violet-light-0 disabled:border-slate-300 disabled:bg-slate-300 lg:mt-8"
-          //   onClick={handleDelete}
+          onClick={handleDelete}
         >
           Oui
         </button>
