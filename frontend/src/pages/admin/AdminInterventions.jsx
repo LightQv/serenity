4f30411734b2886ab3cf4da9ba9a4b976546bf65
Wilder <lilianbataille6263@gmail.com>
2023-06-25 02:57:30 +0200

    Affichage page Gestion des interventions

---
+++ frontend/src/pages/admin/AdminInterventions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AdminInterventions() {
+  return (
+    <main className="min-w-screen relative flex min-h-screen flex-col bg-slate-50 p-4 font-poppins lg:py-16 lg:pl-72 lg:pr-12">
+      <div className="flex w-full items-center justify-between">
+        <h3 className="mb-2 text-2xl font-semibold lg:mb-8 lg:text-4xl">
+          Gestion des interventions
+        </h3>
+      </div>
+    </main>
+  );
+}
