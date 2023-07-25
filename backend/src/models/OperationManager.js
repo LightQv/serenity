6916dc96a032cb5a7e6dfedcd1a581290b4b0ed0
LightQv <QVivian23@gmail.com>
2023-07-25 16:20:35 +0200

    user is back

--- backend/src/models/OperationManager.js
+++ backend/src/models/OperationManager.js
@@ -5,6 +5,18 @@ class OperationManager extends AbstractManager {
     super({ table: "operation" });
   }
 
+  findWithProtocolInfos(id) {
+    return this.database.query(
+      `SELECT protocol.id as protocol_id, protocol_name, color_theme, operation_id, operation_name, COUNT(item.id) as item_count, COUNT(case when is_complete then 1 end) as item_complete
+      FROM ${this.table} 
+      JOIN protocol on operation.id = protocol.operation_id 
+      JOIN protocol_item as item on protocol.id = item.protocol_id 
+      WHERE operation.id = ?
+      GROUP BY protocol_id ORDER BY protocol_id ASC`,
+      [id]
+    );
+  }
+
   insert(operation) {
     return this.database.query(
       `INSERT INTO ${this.table} (operation_name) VALUES (?)`,
