73fe03410abec8015a3f25d17d8a25db4f0a73d3
LightQv <QVivian23@gmail.com>
2023-07-26 12:07:06 +0200

    fix css

--- frontend/src/pages/admin/AdminPatients.jsx
+++ frontend/src/pages/admin/AdminPatients.jsx
@@ -58,7 +58,7 @@ export default function AdminPatients() {
   };
 
   return (
-    <main className="relative flex min-h-screen flex-col bg-slate-50 p-4 font-poppins lg:py-12 lg:pl-72 lg:pr-12">
+    <main className="relative mb-12 flex min-h-screen flex-col bg-slate-50 p-4 font-poppins lg:mb-0 lg:py-12 lg:pl-72 lg:pr-12">
       <div className="flex w-full items-center justify-between">
         <h3 className="mb-2 text-2xl font-semibold lg:mb-8 lg:text-4xl">
           Gestion des patients
